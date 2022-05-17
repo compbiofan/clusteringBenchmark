4f7dd987cd7c745485fe0d0427c815f13a8a9ca5
Rituparna Khan <rk18g@h22-login-25.rcc.fsu.edu>
2022-05-17 15:16:10 -0400

    1. Added one gen_jobs.py for all. 2. Automated eval process for all files.

---
+++ gen_jobs.py
@@ -0,0 +1,88 @@
+import argparse 
+
+# Read each line after # in args.input and include them in the slurm file.
+# And include the header
+def create_job_file(fname,n,mpc,p,cluster_algo):
+    if cluster_algo == 'bnpc':
+        with open(fname,'r') as f:
+            lines = f.readlines()
+
+        count = 0
+        op_fname = (fname.split("."))[0]
+        for line in lines:
+            if "#" in line:
+                continue
+            line_items = line.split(" ")
+            opdir_name = (line_items[8].strip()).replace(".","")
+            print(opdir_name)
+            with open(op_fname+"."+str(count)+".slurm",'w') as f:
+                f.write("#!/bin/bash\n")
+                f.write("#SBATCH --job-name=bnpc_job"+str(count)+"\n")
+                f.write("#SBATCH -N 1\n")
+                f.write("#SBATCH -n "+str(n)+"\n")
+                f.write("#SBATCH --mem-per-cpu="+mpc+"\n")
+                f.write("#SBATCH -p "+p+"\n")
+                f.write("#SBATCH --mail-type=ALL\n")
+                f.write("#SBATCH --output=/gpfs/research/fangroup/rk18g/longitudinal/clustering/bnpc"+opdir_name+"/bnpc_output"+str(count)+".out\n")
+                f.write("#SBATCH --error=/gpfs/research/fangroup/rk18g/longitudinal/clustering/bnpc"+opdir_name+"/bnpc_error"+str(count)+".out\n")
+                f.write(line)
+            count=count+1
+    elif cluster_algo == 'scclone':
+        with open(fname,'r') as f:
+            lines = f.readlines()
+
+        count = 0
+        op_fname = (fname.split("."))[0]
+        for line in lines:
+            if "#" in line:
+                continue
+            line_items = line.split(" ")
+            opdir_name = (((line_items[9].strip()).replace(".","")).split("/"))[2]
+            #print(opdir_name)
+            with open(op_fname+"."+str(count)+".slurm",'w') as f:
+                f.write("#!/bin/bash\n")
+                f.write("#SBATCH --job-name=scclone_job"+str(count)+"\n")
+                f.write("#SBATCH -N 1\n")
+                f.write("#SBATCH -n "+str(n)+"\n")
+                f.write("#SBATCH --mem-per-cpu="+mpc+"\n")
+                f.write("#SBATCH -p "+p+"\n")
+                f.write("#SBATCH --mail-type=ALL\n")
+                f.write("#SBATCH --output=/gpfs/research/fangroup/rk18g/longitudinal/clustering/scclone/output/"+opdir_name+"/scclone_output"+str(count)+".out\n")
+                f.write("#SBATCH --error=/gpfs/research/fangroup/rk18g/longitudinal/clustering/scclone/output/"+opdir_name+"/scclone_error"+str(count)+".out\n")
+                f.write(line)
+            count=count+1
+    elif cluster_algo == 'scg':
+        with open(fname,'r') as f:
+            lines = f.readlines()
+
+        count = 10
+        op_fname = (fname.split("."))[0]
+        for line in lines:
+            if "#" in line:
+                continue
+            line_items = line.split(" ")
+            #opdir_name = (((line_items[3].strip()).replace(".","")).split("/"))[2]
+            opdir_name = line_items[3].strip()
+            print(opdir_name)
+            with open(op_fname+"."+str(count)+".slurm",'w') as f:
+                f.write("#!/bin/bash\n")
+                f.write("#SBATCH --job-name=scg_job"+str(count)+"\n")
+                f.write("#SBATCH -N 1\n")
+                f.write("#SBATCH -n "+str(n)+"\n")
+                f.write("#SBATCH --mem-per-cpu="+mpc+"\n")
+                f.write("#SBATCH -p "+p+"\n")
+                f.write("#SBATCH --mail-type=ALL\n")
+                f.write("#SBATCH --output=/gpfs/research/fangroup/rk18g/longitudinal/clustering/scg/"+opdir_name+"/scg_output"+str(count)+".out\n")
+                f.write("#SBATCH --error=/gpfs/research/fangroup/rk18g/longitudinal/clustering/scg/"+opdir_name+"/scg_error"+str(count)+".out\n")
+                f.write(line)
+            count=count+1
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-input", "--input",dest ="input", help="Input .sh file")
+parser.add_argument("-n","--n",dest="n", help="No of cpus")
+parser.add_argument("-mem_per_cpu", "--mem_per_cpu",dest ="mem_per_cpu", help="Memory per CPU")
+parser.add_argument("-p", "--p",dest ="p", help="partition")
+parser.add_argument("-algo", "--algo",dest ="algo", help="Which cluster algo you are running")
+args = parser.parse_args()
+
+create_job_file(args.input, args.n, args.mem_per_cpu, args.p, args.algo)
